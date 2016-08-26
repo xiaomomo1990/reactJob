2372c60c9dc2a2d95534b573851832d4cdfd0023
莫光勇 <442083156@qq.com>
2016-08-26 16:22:14 +0800

    react job

---
+++ public/scripts/tutorial1.js
@@ -0,0 +1,9 @@
+﻿var CommentBox = React.createClass({
+    render: function () {
+        return (
+            <div className="commentBox">
+                Hello, world! I am a CommentBox.
+            </div>
+        );
+    }
+});
